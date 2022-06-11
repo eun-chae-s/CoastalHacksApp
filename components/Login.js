da6f265bc94324eceb4dc434a72fcc71cb1247d9
MinGi Kwon <mingikwon@MinGis-MacBook-Air.local>
2022-06-12 05:12:33 +0800

    added basic dummy data for register and logging in

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,43 @@
-import React, { Component } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import React, { useState, createRef }  from 'react'
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import dummy_data from '../dummydata';
+
+
+
 
 const Login = ({navigation}) => {
+
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+
+    const emailInputRef = createRef();
+    const passwordInputRef = createRef();
+
+    const handleSubmitButton = () => {
+        let temp = {
+            email: userEmail,
+            password: userPassword
+        }
+        console.log(temp)
+        console.log(dummy_data)
+        if (containsUser(temp)){
+            navigation.navigate('NewsL')
+        } else {
+            alert('Invalid username or password')
+        }
+    }
+
+    const containsUser = (loginDetails) => {
+        for (const user of dummy_data) {
+            console.log(JSON.stringify(loginDetails) === JSON.stringify(user))
+            if (JSON.stringify(loginDetails) === JSON.stringify(user)){
+                return true
+            }
+        }
+        return false
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.header}>
@@ -9,21 +45,31 @@ const Login = ({navigation}) => {
             </Text>
             <TextInput
                 style={styles.textinput_nickname}
-                placeholder="nickname"
+                placeholder="email"
+                onChangeText={(userEmail) => setUserEmail(userEmail)}
+                ref={emailInputRef}
+                returnKeyType="next"
+                            onSubmitEditing={() =>
+                                passwordInputRef.current &&
+                                passwordInputRef.current.focus()
+                            }
             />
             <TextInput
                 style={styles.textinput_pw}
                 placeholder="password"
+                onChangeText={(userPassword) => setUserPassword(userPassword)}
                 secureTextEntry={true}
+                ref={passwordInputRef}
             />
             <View style={styles.button}>
                 <Button
                     title="Login"
-                    color="white"
-                    onPress={() => navigation.navigate('NewsL')}
-                />
+                    color="#deb887"
+                    onPress={handleSubmitButton}
+                >
+                    <Text style={{color: 'black'}}>Login</Text>
+                </Button>
             </View>
-            
         </View>
     );
 }
