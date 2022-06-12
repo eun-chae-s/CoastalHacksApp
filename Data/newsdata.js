f86c53e66351833135ec6a8fcf01d33311f7dba7
Eunchae Seong <83269618+eun-chae-s@users.noreply.github.com>
2022-06-12 19:33:00 +0900

    Add files via upload

---
+++ Data/newsdata.js
@@ -0,0 +1,9 @@
+const newsData = [
+    {'beachname': 'Maui', 'news': 'jellyfish-found!', 'time': '30 min ago'},
+    {'beachname': 'Kauai', 'news': 'jellyfish-found!', 'time': '1 hour ago'},
+    {'beachname': 'Destin', 'news': 'jellyfish-found!', 'time': '2 hours ago'},
+    {'beachname': 'Sanibel Island', 'news': 'jellyfish-found!', 'time': '3 hours ago'},
+    {'beachname': 'Honolulu', 'news': 'jellyfish-found!', 'time': '3 hours ago'},
+];
+
+export default newsData;
